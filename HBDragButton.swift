9a892363c000dff91b28833616ec15150264b1e5
HugoBoscDucros <hugo@padambus.com>
2016-05-13 18:06:49 +0200

    style versionning

--- HBDragButton.swift
+++ HBDragButton.swift
@@ -21,28 +21,73 @@ class HBDragButton: UIView {
     var translationX:CGFloat = 0
     var isDragged = false
     var initalFrame:CGRect!
-    //var minWidth:CGFloat!
+    var slideStyle = false
+    var endStyle = ""
+    var maxAnimationDuration:Double = 1
+    
+    
+// MARK: - Settings
+    
+    func autoSet() {
+        let widthConstraint = NSLayoutConstraint(item: self.draggableAreaView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: self.draggableAreaView.frame.size.width)
+        let heightConstraint = NSLayoutConstraint(item: self.draggableAreaView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: self.draggableAreaView.frame.size.height)
+        print(self.draggableAreaView.frame)
+        let leftConstraint = NSLayoutConstraint(item: self, attribute: .Leading, relatedBy: .Equal, toItem: self.draggableAreaView, attribute: .Leading, multiplier: 1, constant: self.draggableAreaView.frame.origin.x)
+        let topConstraint = NSLayoutConstraint(item: self, attribute: .Top, relatedBy: .Equal, toItem: self.draggableAreaView, attribute: .Top, multiplier: 1, constant: self.draggableAreaView.frame.origin.y)
+        
+        self.draggableAreaView.addConstraint(widthConstraint)
+        self.draggableAreaView.addConstraint(heightConstraint)
+        if !slideStyle {
+            self.addConstraint(leftConstraint)
+        }
+        self.addConstraint(topConstraint)
+        
+        self.layer.cornerRadius = self.frame.size.height/2
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        
+        if slideStyle {
+            self.initalFrame = self.draggableAreaView.frame
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(HBDragButton.dragViewSlided))
+            self.draggableAreaView.addGestureRecognizer(panGesture)
+        } else {
+            self.initalFrame = self.frame
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(HBDragButton.dragViewDragged))
+            self.draggableAreaView.addGestureRecognizer(panGesture)
+            self.translatesAutoresizingMaskIntoConstraints = true
+        }
+    }
+    
+    func slidableStyle() {
+        self.slideStyle = true
+        self.autoSet()
+    }
     
 
 // MARK: - Action
     
     @IBAction func dragViewDragged(sender: UIPanGestureRecognizer) {
+        print("start width = \(self.frame.size.width)")
         let translation = sender.translationInView(self)
         let minWidth = self.draggableAreaView.frame.origin.x * 2 + self.draggableAreaView.frame.size.width
         let minOriginX = self.initalFrame.origin.x + self.initalFrame.size.width - minWidth
         if sender.state == .Began {
             self.translationX = 0
-            
+            self.isDragged = false
         } else if sender.state == .Ended {
             if !self.isDragged {
-                UIView.animateWithDuration(0.5) {
+                let iniWidth = self.initalFrame.size.width
+                let width = self.frame.width
+                UIView.animateWithDuration(Double((iniWidth - width)/(iniWidth - minWidth)) * self.maxAnimationDuration, delay: 0, options: .CurveEaseOut, animations: {
                     self.frame = self.initalFrame
-                }
+                    }, completion: nil)
             }
         } else {
             if translation.x > 0 && self.frame.size.width > minWidth {
                 let m = translation.x - self.translationX
                 self.translationX = translation.x
+                //print("width = \(self.frame.size.width)")
                 if self.frame.size.width - m >= minWidth {
                     self.frame = CGRectMake(self.frame.origin.x + m, self.frame.origin.y, self.frame.size.width - m, self.frame.size.height)
                 } else {
@@ -53,7 +98,8 @@ class HBDragButton: UIView {
                     if self.delegate != nil {
                         self.delegate!.dragButtonCompleteDragging(self)
                     }
-                   print("Well Done !!!")
+                    print("Well Done !!!")
+                    //print("width = \(self.frame.size.width)")
                     
                     //TODO make style modification & ending animation for complete dragging
                     //self.dragView.backgroundColor = padamRed
@@ -61,39 +107,94 @@ class HBDragButton: UIView {
                 }
             }
         }
+        print("start width = \(self.frame.size.width)")
+    }
+    
+    @IBAction func dragViewSlided(sender: UIPanGestureRecognizer) {
+        let translation = sender.translationInView(self)
+        let maxOriginX = self.frame.size.width - self.draggableAreaView.frame.size.width - self.initalFrame.origin.x
+        if sender.state == .Began {
+            self.translationX = 0
+            //self.isDragged = false
+        } else if sender.state == .Ended {
+            if !self.isDragged {
+                let initX = self.initalFrame.origin.x
+                let originX = self.draggableAreaView.frame.origin.x
+                UIView.animateWithDuration(Double((initX - originX)/(initX - maxOriginX)) * self.maxAnimationDuration, delay: 0, options: .CurveEaseOut, animations: {
+                    self.draggableAreaView.frame = self.initalFrame
+                    }, completion: nil)
+            }
+        } else {
+            if translation.x > 0 {
+                let m = translation.x - self.translationX
+                self.translationX = translation.x
+                if self.draggableAreaView.frame.origin.x + m <= maxOriginX {
+                    self.draggableAreaView.frame = CGRectMake(self.draggableAreaView.frame.origin.x + m, self.draggableAreaView.frame.origin.y, self.draggableAreaView.frame.size.width, self.draggableAreaView.frame.size.height)
+                } else {
+                    self.draggableAreaView.frame = CGRectMake(maxOriginX, self.draggableAreaView.frame.origin.y, self.draggableAreaView.frame.size.width, self.draggableAreaView.frame.size.height)
+                }
+                    
+                if self.draggableAreaView.frame.origin.x >= maxOriginX {
+                    if self.delegate != nil {
+                        self.delegate!.dragButtonCompleteDragging(self)
+                    }
+                    print("Well Done !!! \(self.draggableAreaView.frame.origin.x) && \(maxOriginX)")
+                        
+                    //TODO make style modification & ending animation for complete dragging
+                    //self.dragView.backgroundColor = padamRed
+                    if self.endStyle != "ComingBack" {
+                        self.isDragged = true
+                    }
+                }
+            }
+        }
     }
     
     func backToInitialFrame(animated:Bool) {
-        self.isDragged = false
-        if animated {
-            UIView.animateWithDuration(0.5) {
-                self.frame = self.initalFrame
+        if self.isDragged == true {
+            self.isDragged = false
+            if self.slideStyle == true {
+                if animated {
+                    UIView.animateWithDuration(self.maxAnimationDuration, delay: 0, options: .CurveEaseOut, animations: {
+                        self.draggableAreaView.frame = self.initalFrame
+                        }, completion: nil)
+                } else {
+                    self.draggableAreaView.frame = self.initalFrame
+                }
+            } else {
+                if animated {
+                    UIView.animateWithDuration(self.maxAnimationDuration, delay: 0, options: .CurveEaseOut, animations: {
+                        self.frame = self.initalFrame
+                        }, completion: nil)
+                } else {
+                    self.frame = self.initalFrame
+                }
             }
-        } else {
-            self.frame = self.initalFrame
         }
     }
     
      // Only override drawRect: if you perform custom drawing.
      // An empty implementation adversely affects performance during animation.
-     override func drawRect(rect: CGRect) {
-        let widthConstraint = NSLayoutConstraint(item: self.draggableAreaView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: self.draggableAreaView.frame.size.width)
-        let heightConstraint = NSLayoutConstraint(item: self.draggableAreaView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: self.draggableAreaView.frame.size.height)
-        print(self.draggableAreaView.frame)
-        let leftConstraint = NSLayoutConstraint(item: self, attribute: .Leading, relatedBy: .Equal, toItem: self.draggableAreaView, attribute: .Leading, multiplier: 1, constant: self.draggableAreaView.frame.origin.x)
-        let topConstraint = NSLayoutConstraint(item: self, attribute: .Top, relatedBy: .Equal, toItem: self.draggableAreaView, attribute: .Top, multiplier: 1, constant: self.draggableAreaView.frame.origin.y)
-        
-        self.draggableAreaView.addConstraint(widthConstraint)
-        self.draggableAreaView.addConstraint(heightConstraint)
-        self.addConstraint(leftConstraint)
-        self.addConstraint(topConstraint)
-        
-        self.initalFrame = self.frame
-        //self.minWidth = self.draggableAreaView.frame.origin.x * 2 + self.draggableAreaView.frame.size.width
-        //self.translatesAutoresizingMaskIntoConstraints = true
-        
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(HBDragButton.dragViewDragged))
-        self.draggableAreaView.addGestureRecognizer(panGesture)
-     }
+//     override func drawRect(rect: CGRect) {
+//        let widthConstraint = NSLayoutConstraint(item: self.draggableAreaView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: self.draggableAreaView.frame.size.width)
+//        let heightConstraint = NSLayoutConstraint(item: self.draggableAreaView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: self.draggableAreaView.frame.size.height)
+//        print(self.draggableAreaView.frame)
+//        let leftConstraint = NSLayoutConstraint(item: self, attribute: .Leading, relatedBy: .Equal, toItem: self.draggableAreaView, attribute: .Leading, multiplier: 1, constant: self.draggableAreaView.frame.origin.x)
+//        let topConstraint = NSLayoutConstraint(item: self, attribute: .Top, relatedBy: .Equal, toItem: self.draggableAreaView, attribute: .Top, multiplier: 1, constant: self.draggableAreaView.frame.origin.y)
+//        
+//        self.draggableAreaView.addConstraint(widthConstraint)
+//        self.draggableAreaView.addConstraint(heightConstraint)
+//        self.addConstraint(leftConstraint)
+//        self.addConstraint(topConstraint)
+//        
+//        self.layer.cornerRadius = self.frame.size.height/2
+//        
+//        self.initalFrame = self.frame
+//        //self.minWidth = self.draggableAreaView.frame.origin.x * 2 + self.draggableAreaView.frame.size.width
+//        //self.translatesAutoresizingMaskIntoConstraints = true
+//        
+//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(HBDragButton.dragViewDragged))
+//        self.draggableAreaView.addGestureRecognizer(panGesture)
+//     }
     
 }
--- HBDragButtonTestProject/HBDragButtonTestProject/ViewController.swift
+++ HBDragButtonTestProject/HBDragButtonTestProject/ViewController.swift
@@ -11,12 +11,19 @@ import UIKit
 class ViewController: UIViewController, HBDragButtonDelegate {
 
     @IBOutlet weak var dragButton: HBDragButton!
+    @IBOutlet weak var dragButton2: HBDragButton!
+    @IBOutlet weak var dragButton3: HBDragButton!
+    @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dragButton.delegate = self
-        self.dragButton.layer.cornerRadius = 5.0
-        // Do any additional setup after loading the view, typically from a nib.
+        self.dragButton.autoSet()
+        self.dragButton2.delegate = self
+        self.dragButton2.slidableStyle()
+        self.dragButton3.delegate = self
+        self.dragButton3.slidableStyle()
+        self.dragButton3.endStyle = "ComingBack"
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,17 +31,21 @@ class ViewController: UIViewController, HBDragButtonDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        //self.dragButton.layer.cornerRadius = 38//self.dragButton.frame.size.height/2
-    }
+//    override func viewDidAppear(animated: Bool) {
+//        super.viewDidAppear(animated)
+//        //self.dragButton.layer.cornerRadius = 38//self.dragButton.frame.size.height/2
+//    }
 
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.dragButton.backToInitialFrame(true)
+        self.dragButton2.backToInitialFrame(true)
+        self.label.text = "Let's drag a button !"
+        self.label.textColor = UIColor.blackColor()
     }
 
     func dragButtonCompleteDragging(dragButton: HBDragButton) {
-        //
+        self.label.text = "Well Done !!!"
+        self.label.textColor = UIColor.redColor()
     }
     
 }
